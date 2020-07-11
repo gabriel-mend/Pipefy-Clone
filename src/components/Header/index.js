3b9dc35dcaf1258ae35bbebe95415ec4d151204a
Gabriel <gabriel@Gabriel-Mac-mini.local>
2020-07-11 18:57:05 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Header() {
+  return <Container>
+        <h1>Pipefy Clone</h1>
+  </Container>;
+}
+
+export default Header;
